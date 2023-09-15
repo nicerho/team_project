5578a941cba9ecb45b1f8fb1ca91ccb0ef424b3a
nicerho <131422003+nicerho@users.noreply.github.com>
2023-09-15 17:15:41 +0900

    ㄴㄴ

--- test/src/main/java/test/TestClass.java
+++ test/src/main/java/test/TestClass.java
@@ -2,6 +2,7 @@ package test;
 
 public class TestClass {
 	public static void main(String[] args) {
-
+		String test = "깃헙테스트입니다";
+		System.out.println(test);
 	}
 }
--- test/src/main/java/test/main.java
+++ test/src/main/java/test/main.java
@@ -3,6 +3,6 @@ package test;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		
 	}
 }
